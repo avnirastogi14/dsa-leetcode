f4a035905d3c953e935d0eaf0dc1f387b180615a
Avni Rastogi <149324976+avnirastogi14@users.noreply.github.com>
2026-08-02 16:59:04 +0530

    [81] [Time Beats: 100.00%] [Memory Beats: 46.70%] - LeetPush

---
+++ 81-search-in-rotated-sorted-array-ii/81-Search-in-Rotated-Sorted-Array-II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0, h = n-1;
+
+        while(l<=h)
+        {
+            int m = l+(h-l)/2;
+            if(nums[m]==k) return true;
+            if(nums[l]==nums[m] && nums[h]==nums[m])
+            {
+                l++;
+                h--;
+            }
+
+            else if(nums[l]<=nums[m])
+            {
+                if(nums[l]<=k && k<nums[m]) h=m-1;
+                else l=m+1;
+            }
+            
+            else
+            {
+                if(nums[m]<k && k<=nums[h]) l=m+1;
+                else h=m-1;
+            }
+        }
+        return false;   
+    }
+};
