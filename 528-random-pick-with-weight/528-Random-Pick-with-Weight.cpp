afedcc4885391045d5a899d8a1b08a0495a6090d
Avni Rastogi <149324976+avnirastogi14@users.noreply.github.com>
2026-08-05 20:03:23 +0530

    [528] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 528-random-pick-with-weight/528-Random-Pick-with-Weight.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    vector<int> sum;
+    int tot;
+
+public:
+    Solution(vector<int>& w) {
+        sum.push_back(w[0]);
+        for(int i=1;i<w.size();i++) sum.push_back(sum[i-1]+w[i]);
+
+        tot+=sum[w.size()-1];
+    }
+    
+    int pickIndex() {
+        int num = rand()%tot;
+
+        int l = 0, h = sum.size()-1;
+
+        while(l<h)
+        {
+            int m = l+(h-l)/2;
+
+            if(sum[m]<=num) l = m+1;
+            else h = m;
+        }
+        return l;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
