5ca99c038f2c37dbc5888db92ea78873dd346ccc
Avni Rastogi <149324976+avnirastogi14@users.noreply.github.com>
2026-08-15 22:16:47 +0530

    [474] [Time Beats: 43.99%] [Memory Beats: 44.38%] - LeetPush

--- 474-ones-and-zeroes/474-Ones-and-Zeroes.cpp
+++ 474-ones-and-zeroes/474-Ones-and-Zeroes.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-
-    int help(vector<vector<vector<int>>> &dp, int pos, int zc, int oc, vector<string> &ss)
-    {
-        if(pos==0) return 0;
-        if(dp[pos][zc][oc]!=-1) return dp[pos][zc][oc];
-
-        int z = count(ss[pos-1].begin(),ss[pos-1].end(),'0');
-        int o = count(ss[pos-1].begin(),ss[pos-1].end(),'1');
-
-        int nt = help(dp,pos-1,zc,oc,ss);
-
-        int t = 0;
-        if(z<=zc && o<=oc) t = 1+help(dp,pos-1,zc-z,oc-o,ss);
-
-        return dp[pos][zc][oc]=max(t,nt);
+    int helper(vector<string>& a, int i, int m, int n, vector<vector<vector<int>>>& dp) {
+        if (i == a.size()) return 0;
+        if (dp[i][m][n] != -1) return dp[i][m][n];
+        int z = count(a[i].begin(), a[i].end(), '0');
+        int o = count(a[i].begin(), a[i].end(), '1');
+        int notTake = helper(a, i + 1, m, n, dp);
+        int take = 0;
+        if (m >= z && n >= o) take = 1 + helper(a, i + 1, m - z, n - o, dp);
+        return dp[i][m][n] = max(take, notTake);
     }
 
-    int findMaxForm(vector<string>& strs, int zc, int oc) {
-        int ans = 0;
-        int n = strs.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(zc+1,vector<int>(oc+1,-1)));
-        ans = help(dp,n,zc,oc,strs);
-
-        return ans;
+    int findMaxForm(vector<string>& a, int m, int n) {
+        int sz = a.size();
+        vector<vector<vector<int>>> dp(sz,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        return helper(a, 0, m, n, dp);
     }
 };
